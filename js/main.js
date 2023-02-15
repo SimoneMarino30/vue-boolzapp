8f5828b245c6bd85aed05c441460aed8e4bb15ac
Simone Marino <simo.marino@outlook.it>
2023-02-15 13:37:36 +0100

    Last text and date shown fixed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // **TRACCIA**
 // **Milestone 1**
 // ● Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
-// dall’interlocutore (bianco) assegnando due classi CSS diverse
+// dall’interlocutore (bianco) assegnando due classi CSS diverse ✔
 // ● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
-// nome e immagine di ogni contatto
+// nome e immagine di ogni contatto ✔
 // **Milestone 2**
 // ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
 // messaggi relativi al contatto attivo all’interno del pannello della conversazione
@@ -194,4 +194,13 @@ createApp({
       ],
     };
   },
+
+  methods: {
+    activeImage() {
+      // console.log("hai cliccato");
+      if (sms.status == "received") {
+      }
+    },
+  },
 }).mount("#root");
+// ✔
